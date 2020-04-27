39dad9fc893fb7ad7ab1e33a9bd8c4f5e162ea65
Matt Snyder <slylock2@mac.com>
2020-04-27 07:53:06 -0500

    Added Uselessbot opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareUselessbot.java
@@ -0,0 +1,203 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cIrSeekerSensorV3;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.hardware.IrSeekerSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+public class HardwareUselessbot
+{
+    /* Public OpMode members. */
+    public DcMotor  leftMotor   = null;
+    public DcMotor  rightMotor  = null;
+    public CRServo swivelServo = null;
+    public Servo shoulderServo = null;
+    public Servo    buttonPusher    = null;
+    public AnalogInput potentiometer = null;
+    public ColorSensor colorSensor = null;
+    public ModernRoboticsI2cRangeSensor rangeSensor = null;
+    public IrSeekerSensor irSeekerH;
+    public ModernRoboticsI2cIrSeekerSensorV3 irSeekerV;
+    private I2cDevice irSeekerVDevice;
+    private I2cDeviceSynch irSeekerVreader;
+
+    public static final double PUSHER_RETRACT   =  0.5 ;
+    public static final double PUSHER_EXTEND   =  0.2 ;
+    public static final double ARM_STOP_POWER    =  0.0 ;
+    public static final double ARM_RIGHT_POWER  = 1.0 ;
+    public static final double ARM_LEFT_POWER  = -1.0 ;
+    public static final double SHOULDER_LEVEL = .15;
+    public static final double ARM_FRONT = 34.3;
+    public static final double ARM_RIGHT = 38.1;
+    public static final double ARM_REAR = 27.7;
+    public static final double ARM_LEFT = 30.4;
+    public static final double ARM_TOLERANCE = 0.2;
+    public static final double IR_ANGLE_TOLERANCE_H = 70;
+    public static final double IR_ANGLE_TOLERANCE_V = 60;
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+    private boolean ReverseDriveMotors = false;
+
+    /* Constructor */
+    public HardwareUselessbot(){
+    }
+
+    public HardwareUselessbot(boolean reverse){
+        ReverseDriveMotors = reverse;
+    }
+
+    // "complete" solution from Nick Spence for #360, multiple same I2C sensors
+    private void setI2cAddress(ModernRoboticsI2cIrSeekerSensorV3 sensor, int addr)
+    {
+        sensor.setI2cAddress(I2cAddr.create8bit(addr));
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftMotor   = hwMap.dcMotor.get("left_drive");
+        rightMotor  = hwMap.dcMotor.get("right_drive");
+        // MJS was... armMotor    = hwMap.dcMotor.get("left_arm");
+
+        if (ReverseDriveMotors) {
+            leftMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+            rightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        } else {
+            leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+            rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        }
+
+        // Set all motors to zero power
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        swivelServo = hwMap.crservo.get("swivel");
+        buttonPusher = hwMap.servo.get("pusher");
+        buttonPusher.setPosition(PUSHER_RETRACT);
+        shoulderServo = hwMap.servo.get("shoulder");
+        shoulderServo.setPosition(SHOULDER_LEVEL);
+
+        // Sensors
+        //rangeSensor = hwMap.get(ModernRoboticsI2cRangeSensor.class, "range");
+        potentiometer = hwMap.analogInput.get("potentiometer");
+        //colorSensor = hwMap.colorSensor.get("color");
+        //colorSensor.enableLed(false);
+        irSeekerH = hwMap.irSeekerSensor.get("seekerH");
+        //irSeekerV = hwMap.irSeekerSensor.get("seekerV");
+        //irSeekerV.setI2cAddress(I2cAddr.create8bit(0x42));
+
+        // "complete" solution from Nick Spence for #360, multiple same I2C sensors
+        irSeekerVDevice = hwMap.i2cDevice.get("seekerV");
+        irSeekerVreader = new I2cDeviceSynchImpl(irSeekerVDevice, I2cAddr.create8bit(0x0), false); // was I2cDeviceSynchImpl2
+        irSeekerV = new ModernRoboticsI2cIrSeekerSensorV3(irSeekerVreader);
+
+        // one IR sensor has had address changed
+        setI2cAddress(irSeekerV, 0x42);  // "complete" solution from Nick Spence for #360, multiple same I2C sensors
+
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     * @throws InterruptedException
+     */
+    public void waitForTick(long periodMs) throws InterruptedException {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+    public double armPosition() {
+        double voltreading = (float) potentiometer.getVoltage();
+        double percentTurned = voltreading/5 * 100;
+        return percentTurned;
+    }
+
+    public double IRMovementH() {
+        double currentPos = armPosition();
+        double angle = irSeekerH.getAngle();
+        double result = ARM_STOP_POWER;
+        if (Math.abs(angle) > IR_ANGLE_TOLERANCE_H) {
+            if (angle > 0 && currentPos < ARM_RIGHT)
+                result = ARM_RIGHT_POWER;
+            else if (currentPos > ARM_REAR)
+                result = ARM_LEFT_POWER;
+        }
+        return result;
+    }
+
+    public double IRMovementV() { // positive, negative, or zero
+        double angle = irSeekerV.getAngle();
+        double result = 0.0;
+        if (Math.abs(angle) > IR_ANGLE_TOLERANCE_V) {
+            //result = angle < 0 ? 1 : -1;
+            result = -angle / (IR_ANGLE_TOLERANCE_V * 2);
+        }
+        return result;
+    }
+
+    public double powerToPosition(double target) {
+        double currentPos = armPosition();
+        double result = ARM_STOP_POWER;
+        if (Math.abs(currentPos - target) > ARM_TOLERANCE) {
+            if (currentPos > ARM_RIGHT) { // don't swing any further right; go back
+                result = ARM_LEFT_POWER;
+            } else if (currentPos < ARM_REAR) { // don't swing any further left; go back
+                result = ARM_RIGHT_POWER;
+            } else { // ok to swing either way
+                if (target > currentPos)
+                    result = ARM_RIGHT_POWER;
+                else
+                    result = ARM_LEFT_POWER;
+            }
+        }
+        return result;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UselessbotTeleop.java
@@ -0,0 +1,151 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.Range;
+
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_FRONT;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_LEFT;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_LEFT_POWER;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_REAR;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_RIGHT;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_RIGHT_POWER;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.ARM_STOP_POWER;
+import static org.firstinspires.ftc.teamcode.HardwareUselessbot.SHOULDER_LEVEL;
+
+/**
+ * This file provides basic Telop driving for a Pushbot robot.
+ * The code is structured as an Iterative OpMode
+ *
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ *
+ * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Uselessbot Teleop", group="Bots")
+//@Disabled
+public class UselessbotTeleop extends OpMode{
+
+    /* Declare OpMode members. */
+    HardwareUselessbot robot       = new HardwareUselessbot(); // use the class created to define a Pushbot's hardware
+    // could also use HardwarePushbotMatrix class.
+    double          shoulderPos  = SHOULDER_LEVEL ;                  // Servo mid position
+    final double          shoulderInc = 0.008;
+    //final double swivelInc = 0.01;
+    double shoulderTime= 0.0;
+    double swivelTime = 0.0;
+    final double shoulderTimeDelay = 0.1;
+    final double swivelTimeDelay = 0.1;
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        updateTelemetry(telemetry);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double left;
+        double right;
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        left = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+        robot.leftMotor.setPower(left);
+        robot.rightMotor.setPower(right);
+
+        // Use gamepad left & right Bumpers to swivel
+        if (gamepad1.right_bumper)
+            robot.swivelServo.setPower(ARM_RIGHT_POWER);
+        else if (gamepad1.left_bumper)
+            robot.swivelServo.setPower(ARM_LEFT_POWER);
+
+            // use gamepad D buttons to swing to specific compass points
+        else if (gamepad1.dpad_up)
+            robot.swivelServo.setPower(robot.powerToPosition(ARM_FRONT));
+        else if (gamepad1.dpad_right)
+            robot.swivelServo.setPower(robot.powerToPosition(ARM_RIGHT));
+        else if (gamepad1.dpad_down)
+            robot.swivelServo.setPower(robot.powerToPosition(ARM_REAR));
+        else if (gamepad1.dpad_left)
+            robot.swivelServo.setPower(robot.powerToPosition(ARM_LEFT));
+
+            // use A (green) and Y (yellow) to raise and lower shoulder
+        else if (gamepad1.y && getRuntime() > shoulderTime + shoulderTimeDelay) {
+            robot.shoulderServo.setPosition(shoulderPos += shoulderInc);
+            shoulderTime = getRuntime();
+        } else if (gamepad1.a && getRuntime() > shoulderTime + shoulderTimeDelay) {
+            robot.shoulderServo.setPosition(shoulderPos -= shoulderInc);
+            shoulderTime = getRuntime();
+        }
+
+        // no buttons; check for IR
+        else if (robot.irSeekerH.signalDetected()) {
+            robot.swivelServo.setPower(robot.IRMovementH());
+            if (robot.irSeekerV.signalDetected() && getRuntime() > shoulderTime + shoulderTimeDelay) {
+                robot.shoulderServo.setPosition(shoulderPos += robot.IRMovementV() * shoulderInc);
+                shoulderTime = getRuntime();
+            }
+        }
+
+        else { // no buttons or IR; stop swivel
+            robot.swivelServo.setPower(ARM_STOP_POWER);
+        }
+
+        // Send telemetry message to signify robot running;
+        telemetry.addData("AngleH",    robot.irSeekerH.getAngle());
+        telemetry.addData("PercentRot", "percent: " + Double.toString(robot.armPosition()));
+        telemetry.addData("left",  "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+        telemetry.addData("encoders",  "%7d - %7d",
+                robot.leftMotor.getCurrentPosition(), robot.rightMotor.getCurrentPosition());
+        updateTelemetry(telemetry);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+
+
+}
